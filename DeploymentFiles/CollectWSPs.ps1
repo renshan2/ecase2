2b0573694903b67a15b9353df9fd3218c369cc27
Ren Shan <rshan@cabinjohnconsulting.com>
2019-04-24 21:40:13 -0400

    remove deployment folder

--- DeploymentFiles/CollectWSPs.ps1
+++
@@ -1,7 +0,0 @@
-copy ..\Code\Treasury.ECM.eCase.SD.DOI.Common\bin\Release\*.wsp .\
-copy ..\Code\Treasury.ECM.eCase.SD.DOI.Extensions\bin\Release\*.wsp .\
-copy ..\Code\Treasury.ECM.eCase.SD.DOI.Logging\bin\Release\*.wsp .\
-copy ..\Code\Treasury.ECM.eCase.SD.DOI.search\bin\Release\*.wsp .\
-copy ..\Code\Treasury.ECM.eCase.SD.DOI.Site\bin\Release\*.wsp .\
-copy ..\Code\Treasury.ECM.eCase.SD.DOI.Web\bin\Release\*.wsp .\
-
